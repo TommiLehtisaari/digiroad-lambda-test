f6184efee7b9902d2267e974fa9912fef5b19370
Tommi Lehtisaari <tommi@Tommi-MacBook-Pro.local>
2020-01-24 14:37:50 +0200

    irrelevant demo commit.

--- lambdas/upload.js
+++ lambdas/upload.js
@@ -1,5 +1,5 @@
 import { success } from '../libs/response-lib';
 
 export async function main(event) {
-  return success({ message: 'Update method is not yet implemented' });
+  return success({ message: 'Update not implemented.' });
 }
